8b899a699fde67f0df588e98d86e7fb029c29986
Stepan <stepan.de@mail.ru>
2022-09-26 19:40:00 +0300

    All files added

---
+++ main.py
@@ -0,0 +1,54 @@
+import tornado.ioloop
+import tornado.web
+import os 
+
+def resultat(first_num, op, second_num):
+    if op == "+":
+        return first_num + second_num
+    elif op == "-":
+        return first_num - second_num
+    elif op == "*":
+        return first_num * second_num
+    elif op == "/":
+        if second_num != 0:
+            return first_num/second_num
+        else: 
+            return "На ноль делить нельзя"
+    else:
+        return "Такой опреации не существует"
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("result.html", result = None)
+
+class ResultHandler(tornado.web.RequestHandler):
+    def post(self):
+        fisrt_number = self.get_argument("first_num", default=None, strip=False)
+        second_number = self.get_argument("second_num", default=None, strip=False)
+        operation = self.get_argument("operation", default=None, strip=False)
+        try:
+            print(fisrt_number, second_number, operation)
+            result = resultat(int(fisrt_number), operation, int(second_number))
+        except Exception as err:
+            self.render("result.html", result="Неверно введены данные, попробуйте ещё раз")
+        self.render("result.html", result=result)
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+        (r"/result", ResultHandler),
+    ], 
+    debug = True,
+    autoreload = True,
+    static_path = os.path.join(os.path.dirname(__file__), "static"))
+
+if __name__ == "__main__":
+    app = make_app()
+    port = 8888
+    app.listen(port)
+    print(f'Server is listening on localhost on port {8888}')
+    tornado.ioloop.IOLoop.current().start()
+
+
+
+
---
+++ test_main.py
@@ -0,0 +1,11 @@
+from main import resultat
+
+def tests_fn():
+    assert resultat(132, "+", 314) == 446
+    assert resultat(132, "+", 14) == 146
+    assert resultat(12, "*", 3) == 63
+    assert resultat(10, "*", 20) == 200
+    assert resultat(42, "-", 22) == 20
+    assert resultat(33, "-", 11) == 22
+    assert resultat(66, "/", 6) == 11
+    assert resultat(64, "/", 2) == 32
