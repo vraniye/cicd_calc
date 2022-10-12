ef024225c64a09b27417961399a14dc99764be9f
Stepan <stepan.de@mail.ru>
2022-10-12 15:51:30 +0300

    Testing flake8 v2

--- src/main.py
+++ src/main.py
@@ -24,38 +24,34 @@ class MainHandler(tornado.web.RequestHandler):
         self.render("result.html", result=None)
 
 
-
 class ResultHandler(tornado.web.RequestHandler):
     def post(self):
         fisrt_number = self.get_argument("first_num",
-        default=None, strip=False)
+            default=None, strip=False)
         second_number = self.get_argument("second_num",
-        default=None, strip=False)
+            default=None, strip=False)
         operation = self.get_argument("operation", default=None, strip=False)
         try:
             result = resultat(int(fisrt_number), operation, int(second_number))
             self.render("result.html", result=result)
-        except Exception as err:
+        except Exception:
             self.render("result.html",
-            result="Неверно введены данные, попробуйте ещё раз")
+                result="Неверно введены данные, попробуйте ещё раз")
 
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
         (r"/result", ResultHandler),
-    ], 
+    ],
     debug = True,
     autoreload = True,
     static_path = os.path.join(os.path.dirname(__file__), "static"))
 
-if __name__ == "__main__":
+
+if __name__=="__main__":
     app = make_app()
     port = 8888
     app.listen(port)
     print(f'Server is listening on localhost on port {8888}')
     tornado.ioloop.IOLoop.current().start()
-
-
-
-
--- tests/test_main.py
+++ tests/test_main.py
@@ -9,10 +9,11 @@ def test_1():
             '_xsrf': '2|a854d631|73132441a7cf7d4a38279244e7b2c43a|1665387598'
         }
     resultat_html = r.post("http://localhost:8888/result",
-    data).content.decode("utf-8")
+        data).content.decode("utf-8")
     resultat_num = int(resultat_html[resultat_html.index('<span>')
-    +6:resultat_html.index('</span>')-1])
+        + 6:resultat_html.index('</span>')-1])
     assert resultat_num == 5
+
 
 def test_2():
     data = {
@@ -22,10 +23,11 @@ def test_2():
             '_xsrf': '2|a854d631|73132441a7cf7d4a38279244e7b2c43a|1665387598'
         }
     resultat_html = r.post("http://localhost:8888/result",
-    data).content.decode("utf-8")
+        data).content.decode("utf-8")
     resultat_num = int(resultat_html[resultat_html.index('<span>')
-    +6:resultat_html.index('</span>')-1])
+        + 6:resultat_html.index('</span>')-1])
     assert resultat_num == 36
+
 
 def test_3():
     data = {
@@ -35,7 +37,7 @@ def test_3():
             '_xsrf': '2|a854d631|73132441a7cf7d4a38279244e7b2c43a|1665387598'
         }
     resultat_html = r.post("http://localhost:8888/result",
-    data).content.decode("utf-8")
+        data).content.decode("utf-8")
     resultat_num = int(resultat_html[resultat_html.index('<span>')
-    +6:resultat_html.index('</span>')-1])
+        + 6:resultat_html.index('</span>')-1])
     assert resultat_num == 9
